8b082ec63fe94114fe2a561fdd9cdd8a3e3a5466
Barbiewi <barbaradariariveraanguiano@gmail.com>
2025-04-06 12:50:31 -0600

    Administrador de canciones

---
+++ control4h/Components/Data/Canciones.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace control4h.Components.Data
+{
+    public class Canciones
+    {
+        [Required(ErrorMessage = "Agrega el nombre de la cancion")]
+        public string? Cancion { get; set; }
+    }
+}

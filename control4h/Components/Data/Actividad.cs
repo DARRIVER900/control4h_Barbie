6aa903ca8af71ea6e8b89cdff9195b25a62c0a80
Barbiewi <barbaradariariveraanguiano@gmail.com>
2025-04-06 08:57:28 -0600

    Administrador de tareas

---
+++ control4h/Components/Data/Actividad.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace control4h.Components.Data
+{
+    public class Actividad
+    {
+        [Required(ErrorMessage = "Agrega la actividad")]
+        public string? Actividades { get; set; }
+    }
+}
